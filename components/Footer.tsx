134ba88f10d2ee46aecf5c2b2501b16bc399bca6
FhmiAksn <fahmiaksannugroho@gmail.com>
2025-01-08 10:17:10 +0700

    feat:add year automatically

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,8 +11,8 @@ const Footer = () => {
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <Image
-        width={500}
-        height={500}
+          width={500}
+          height={500}
           src="/footer-grid.svg"
           alt="grid"
           className="w-full h-full opacity-50 "
@@ -38,21 +38,21 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Fahmi Aksan Nugroho
+          Copyright © <script>document.write(new Date().getFullYear());</script> Fahmi Aksan Nugroho
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
             <Link
-            href={info.url}
+              href={info.url}
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <Image 
-              src={info.img}
-              alt="icons" 
-              width={20}
-              height={20} />
+              <Image
+                src={info.img}
+                alt="icons"
+                width={20}
+                height={20} />
             </Link>
           ))}
         </div>
